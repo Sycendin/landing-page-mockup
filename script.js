621d896fb11d616f495e6be5f6b234a38580a066
Brandon <37314885+Sycendin@users.noreply.github.com>
2022-02-06 04:02:19 -0500

    adding ability to change backgrounds

---
+++ script.js
@@ -0,0 +1,33 @@
+const {body} = document;
+
+const changeBackground = (number) =>{
+    //Check if background is already showing
+    let previousBackground;
+    if(body.className){
+        previousBackground = body.className;
+    }
+    //Reset background
+    body.className = ''
+    switch(number){
+        case '1':
+            return (previousBackground === 'background-1' ? false : body.classList.add('background-1')); 
+    
+        case '2':
+            return (previousBackground === 'background-2' ? false : body.classList.add('background-2')); 
+ 
+        
+        case '3':
+            return (previousBackground === 'background-3' ? false : body.classList.add('background-3')); 
+ 
+        
+        case '4':
+            return (previousBackground === 'background-4' ? false : body.classList.add('background-4')); 
+
+    
+        case '5':
+            return (previousBackground === 'background-5' ? false : body.classList.add('background-5')); 
+ 
+        default:
+            break;
+    }
+}
